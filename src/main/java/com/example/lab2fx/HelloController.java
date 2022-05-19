9b3ccf83b7e70095201129c6297ea0e86429e26d
Mateusz <MATEUSZ24.PS@GMAIL.COM>
2022-05-19 13:04:41 +0200

    3 pobieranie danych

--- src/main/java/com/example/lab2fx/HelloController.java
+++ src/main/java/com/example/lab2fx/HelloController.java
@@ -1,11 +1,34 @@
 package com.example.lab2fx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private TextField promien;
+    @FXML
+    private TextField wysokosc;
+    @FXML
+    private TextField grubosc;
+    @FXML
+    public Button btnMain;
+
+    @FXML
+    public void onBtnAction(ActionEvent actionEvent){
+        System.out.println("");
+    }
+    int r,h,grub;
+    public void submit(ActionEvent event){
+        r = Integer.parseInt(promien.getText());
+        h = Integer.parseInt(wysokosc.getText());
+        grub = Integer.parseInt(grubosc.getText());
+        System.out.println();
+    }
 
     @FXML
     protected void onHelloButtonClick() {
